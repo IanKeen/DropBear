915e145782efcd5a4d4e5a66224a9d98511fb6bf
Ian Keen <iankeen82@gmail.com>
2019-07-04 17:26:07 -0700

    Update AlertRobot

---
+++ DropBear/Alerts/Alert+RunningRobot.swift
@@ -0,0 +1,36 @@
+import XCTest
+
+extension RunningRobot where Current: AlertRobotType {
+    public func tap(_ button: Current.A.AlertButtonType, file: StaticString = #file, line: UInt = #line) -> Previous {
+        let element = button.buttonElement(from: current.alert)
+
+        let actualElement: XCUIElement
+        if current.required {
+            actualElement = element.assert(.exists, file: file, line: line)
+        } else {
+            actualElement = element.optional()
+        }
+        actualElement.tap()
+
+        return previous
+    }
+}
+
+extension RunningRobot {
+    public func alert<T: AlertButton>(
+        _ alert: Alert<T>,
+        required: Bool = true,
+        file: StaticString = #file, line: UInt = #line
+        ) -> NextRobot<AlertRobot<Alert<T>>>
+    {
+        let dialog = Springboard.application.alerts.firstMatch
+        let actualDialog: XCUIElement
+        if required {
+            actualDialog = dialog.assert(.exists, .contains(alert.text), file: file, line: line)
+        } else {
+            actualDialog = dialog.optional()
+        }
+
+        return .init(current: .init(alert: actualDialog, required: required, app: app), previous: self)
+    }
+}
