b0a11cc83e41e61c917cae4820ad824076983249
Ian Keen <iankeen82@gmail.com>
2019-11-28 11:48:45 -0800

    Refactor app deleting for ios12 + 13

--- DropBear/Springboard.swift
+++ DropBear/Springboard.swift
@@ -1,9 +1,42 @@
 import XCTest
 
+extension Springboard.DeleteAppButton {
+    public static var `default`: Springboard.DeleteAppButton {
+        return .init { application, icon in
+            let ios13button = iOS13.element(application, icon)
+            if ios13button.exists { return ios13button }
+            return iOS12.element(application, icon)
+        }
+    }
+
+    public static var iOS12: Springboard.DeleteAppButton {
+        return .init { application, icon in
+            // find X
+            let iconFrame = icon.frame
+            let iconDeleteOffset = CGVector(
+                dx: (iconFrame.minX + 3) / application.frame.maxX,
+                dy: (iconFrame.minY + 3) / application.frame.maxY
+            )
+
+            application
+                .coordinate(withNormalizedOffset: iconDeleteOffset)
+                .tap()
+
+            return application.alerts.buttons["Delete"]
+        }
+    }
+
+    public static var iOS13: Springboard.DeleteAppButton {
+        return .init { application, _ in
+            return application.buttons["Delete App"]
+        }
+    }
+}
+
 public enum Springboard {
     static let application = XCUIApplication(bundleIdentifier: "com.apple.springboard")
 
-    public static func deleteApp(named name: String, required: Bool = false, file: StaticString = #file, line: UInt = #line) {
+    public static func deleteApp(named name: String, using button: DeleteAppButton = .default, required: Bool = false, file: StaticString = #file, line: UInt = #line) {
         let icon = application.icons[name]
         let iconAvailable = icon.waitForExistence(timeout: DropBear.defaultWaitTime) && icon.isHittable
 
@@ -14,19 +47,11 @@ public enum Springboard {
             return XCTFail("Application icon named '\(name)' not found.", file: file, line: line)
         }
 
+        // Long press
         icon.press(forDuration: 1.5)
 
-        let iconFrame = icon.frame
-        let iconDeleteOffset = CGVector(
-            dx: (iconFrame.minX + 3) / application.frame.maxX,
-            dy: (iconFrame.minY + 3) / application.frame.maxY
-        )
-
-        application
-            .coordinate(withNormalizedOffset: iconDeleteOffset)
-            .tap()
-
-        let deleteButton = application.alerts.buttons["Delete"]
+        // find delete button
+        let deleteButton = button.element(application, icon)
         let deleteButtonAvailable = deleteButton.waitForExistence(timeout: DropBear.defaultWaitTime) && deleteButton.isHittable
 
         guard deleteButtonAvailable else {
@@ -35,5 +60,17 @@ public enum Springboard {
 
         deleteButton.tap()
         XCUIDevice.shared.press(.home)
+    }
+}
+
+extension Springboard {
+    public struct DeleteAppButton {
+        public typealias Input = (_ application: XCUIApplication, _ icon: XCUIElement) -> XCUIElement
+
+        let element: Input
+
+        public init(element: @escaping Input) {
+            self.element = element
+        }
     }
 }
