3f30c7b223bd9c87848ffb818258c59a94832852
Ian Keen <iankeen82@gmail.com>
2020-02-26 16:57:45 -0800

    Add ability to access config from RunningRobot

--- DropBear/RunningRobot/RunningRobot+Root.swift
+++ DropBear/RunningRobot/RunningRobot+Root.swift
@@ -1,11 +1,11 @@
 public enum None: RobotContext { }
 
 extension RunningRobot {
-    public typealias RootRobot<Current: Robot> = RunningRobot<None, Current, Root>
+    public typealias RootRobot<Current: Robot> = RunningRobot<None, Configuration, Current, Root>
     
     public enum RootAction { case root }
 
     public func nextRobot<T: Robot>(_: T.Type = T.self, action: RootAction) -> RootRobot<T> {
-        return RootRobot(current: .init(app: app), previous: .init(app: app))
+        return RootRobot(configuration: configuration, current: .init(app: app), previous: .init(app: app))
     }
 }
--- DropBear/RunningRobot/RunningRobot.swift
+++ DropBear/RunningRobot/RunningRobot.swift
@@ -2,15 +2,21 @@ import XCTest
 
 public protocol RobotContext { }
 
-public class RunningRobot<Context: RobotContext, Current: Robot, Previous: Robot>: Robot {
+public struct NoConfiguration {
+    init() { }
+}
+
+public class RunningRobot<Context: RobotContext, Configuration, Current: Robot, Previous: Robot>: Robot {
     public typealias Element = Current.Element
 
     public var app: XCUIApplication { return current.app }
 
+    public let configuration: Configuration
     public let current: Current
     public let previous: Previous
 
-    public required init(current: Current, previous: Previous) {
+    public required init(configuration: Configuration, current: Current, previous: Previous) {
+        self.configuration = configuration
         self.current = current
         self.previous = previous
     }
