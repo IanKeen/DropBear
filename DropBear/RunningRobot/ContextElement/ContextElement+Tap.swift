fb6993e89fdba5b645af8430d15626738b880ef9
Ian Keen <iankeen82@gmail.com>
2020-03-04 12:17:14 -0800

    Add tap actions for ContextElements

---
+++ DropBear/RunningRobot/ContextElement/ContextElement+Tap.swift
@@ -0,0 +1,37 @@
+import XCTest
+
+extension RunningRobot {
+    @discardableResult
+    public func tap(
+        _ element: ContextElement<Context>,
+        in hierarchy: [XCUIElement.ElementType] = [.any],
+        _ assertion: ElementAssertion,
+        file: StaticString = #file, line: UInt = #line
+        ) -> Self
+    {
+        return tap(element, in: hierarchy, [assertion], file: file, line: line)
+    }
+
+    @discardableResult
+    public func tap(
+        _ element: ContextElement<Context>,
+        in hierarchy: [XCUIElement.ElementType] = [.any],
+        _ assertion: ElementAssertion, _ rest: ElementAssertion...,
+        file: StaticString = #file, line: UInt = #line
+        ) -> Self
+    {
+        return tap(element, in: hierarchy, [assertion] + rest, file: file, line: line)
+    }
+
+    @discardableResult
+    public func tap(
+        _ element: ContextElement<Context>,
+        in hierarchy: [XCUIElement.ElementType] = [.any],
+        _ assertions: [ElementAssertion],
+        file: StaticString = #file, line: UInt = #line
+        ) -> Self
+    {
+        element.element(app).assert(.exists, file: file, line: line).tap()
+        return self
+    }
+}
--- DropBear/RunningRobot/ContextElement/ContextElement.swift
+++ DropBear/RunningRobot/ContextElement/ContextElement.swift
@@ -7,39 +7,3 @@ public struct ContextElement<Context: RobotContext> {
         self.element = element
     }
 }
-
-extension RunningRobot {
-    @discardableResult
-    public func assert(
-        _ element: ContextElement<Context>,
-        in hierarchy: [XCUIElement.ElementType] = [.any],
-        _ assertion: ElementAssertion,
-        file: StaticString = #file, line: UInt = #line
-        ) -> Self
-    {
-        return assert(element, in: hierarchy, [assertion], file: file, line: line)
-    }
-
-    @discardableResult
-    public func assert(
-        _ element: ContextElement<Context>,
-        in hierarchy: [XCUIElement.ElementType] = [.any],
-        _ assertion: ElementAssertion, _ rest: ElementAssertion...,
-        file: StaticString = #file, line: UInt = #line
-        ) -> Self
-    {
-        return assert(element, in: hierarchy, [assertion] + rest, file: file, line: line)
-    }
-
-    @discardableResult
-    public func assert(
-        _ element: ContextElement<Context>,
-        in hierarchy: [XCUIElement.ElementType] = [.any],
-        _ assertions: [ElementAssertion],
-        file: StaticString = #file, line: UInt = #line
-        ) -> Self
-    {
-        _ = element.element(app).assert(assertions, file: file, line: line)
-        return self
-    }
-}
