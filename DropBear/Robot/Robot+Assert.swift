ded365ea658a164c30ddf8004e3b9f78260d7229
Ian Keen <iankeen82@gmail.com>
2020-03-07 14:16:17 -0800

    Add `assert` overloads to assert on multiple elements at once

--- DropBear/Robot/Robot+Assert.swift
+++ DropBear/Robot/Robot+Assert.swift
@@ -39,4 +39,43 @@ extension Robot where Self: Assertable {
 
         return self
     }
+
+    @discardableResult
+    public func assert(
+        _ elements: [Element],
+        in hierarchy: [XCUIElement.ElementType] = [.any],
+        _ assertion: ElementAssertion,
+        file: StaticString = #file, line: UInt = #line
+        ) -> Self
+    {
+        return assert(elements, in: hierarchy, [assertion], file: file, line: line)
+    }
+
+    @discardableResult
+    public func assert(
+        _ elements: [Element],
+        in hierarchy: [XCUIElement.ElementType] = [.any],
+        _ assertion: ElementAssertion, _ rest: ElementAssertion...,
+        file: StaticString = #file, line: UInt = #line
+        ) -> Self
+    {
+        return assert(elements, in: hierarchy, [assertion] + rest, file: file, line: line)
+    }
+
+    @discardableResult
+    public func assert(
+        _ elements: [Element],
+        in hierarchy: [XCUIElement.ElementType] = [.any],
+        _ assertions: [ElementAssertion],
+        file: StaticString = #file, line: UInt = #line
+        ) -> Self
+    {
+        let allElements = elements.map { $0.element(in: source, hierarchy: hierarchy, file: file, line: line) }
+
+        for element in allElements {
+            element.assert(assertions, file: file, line: line)
+        }
+
+        return self
+    }
 }
