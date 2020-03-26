6031251f6337e3b1861ba868b6a65725b7b5d972
Ben Sarrazin <b@srz.io>
2020-03-26 07:40:09 -0400

    Updating package versions

--- DropBearGen/Package.swift
+++ DropBearGen/Package.swift
@@ -1,12 +1,12 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 
 import PackageDescription
 
 let package = Package(
     name: "DropBearGen",
     dependencies: [
-        .package(url: "https://github.com/apple/swift-syntax.git", .exact("0.50100.0")),
-        .package(url: "https://github.com/tid-kijyun/Kanna.git", from: "4.0.0")
+        .package(name: "SwiftSyntax", url: "https://github.com/apple/swift-syntax.git", .exact("0.50200.0")),
+        .package(name: "Kanna", url: "https://github.com/tid-kijyun/Kanna.git", from: "4.0.0")
     ],
     targets: [
         .target(
