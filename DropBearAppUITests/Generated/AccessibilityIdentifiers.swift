506211bfe1080944a2dbe2c2a02d837735ecabc3
Ian Keen <iankeen82@gmail.com>
2021-03-10 18:50:17 -0800

    Update app generated a11y ids

--- DropBearAppUITests/Generated/AccessibilityIdentifiers.swift
+++ DropBearAppUITests/Generated/AccessibilityIdentifiers.swift
@@ -2,24 +2,14 @@
 // Generated using DropBearGen
 
 enum AccessibilityIdentifiers {
-	struct ScrollableStackViewController: RawRepresentable {
+	struct HomeViewController: RawRepresentable {
 		let rawValue: String
-		static let button = ScrollableStackViewController(rawValue: "button")
-		static let button1 = ScrollableStackViewController(rawValue: "button1")
-		static let button2 = ScrollableStackViewController(rawValue: "button2")
-		static let button3 = ScrollableStackViewController(rawValue: "button3")
-		static let label = ScrollableStackViewController(rawValue: "label")
-		static let segment = ScrollableStackViewController(rawValue: "segment")
-		static let slider = ScrollableStackViewController(rawValue: "slider")
-		static let `switch` = ScrollableStackViewController(rawValue: "switch")
-		static let textField = ScrollableStackViewController(rawValue: "textField")
+		static let tabBar = HomeViewController(rawValue: "tabBar")
 	}
-	struct Tab1ViewController: RawRepresentable {
+	struct LoginViewController: RawRepresentable {
 		let rawValue: String
-		static let tab1button = Tab1ViewController(rawValue: "tab1button")
-	}
-	struct Tab2ViewController: RawRepresentable {
-		let rawValue: String
-		static let tab2button = Tab2ViewController(rawValue: "tab2button")
+		static let login = LoginViewController(rawValue: "login")
+		static let password = LoginViewController(rawValue: "password")
+		static let username = LoginViewController(rawValue: "username")
 	}
 }
