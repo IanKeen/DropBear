bbb572dff3b4e4db830effb99b48d259f148b74b
Ian Keen <iankeen82@gmail.com>
2021-03-10 18:49:50 -0800

    Add a11y id to tab

--- DropBearApp/UIKit/Screens/Home/HomeViewController.swift
+++ DropBearApp/UIKit/Screens/Home/HomeViewController.swift
@@ -25,4 +25,8 @@ class HomeViewController: UITabBarController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBar.accessibilityIdentifier = "tabBar"
+    }
 }
