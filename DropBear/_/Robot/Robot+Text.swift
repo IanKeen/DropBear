8dbd03cf0110bc923d7c1b0cd623c733de2978d4
Ian Keen <iankeen82@gmail.com>
2021-03-10 16:15:40 -0800

    Update Robot text interactions

--- DropBear/_/Robot/Robot+Text.swift
+++ DropBear/_/Robot/Robot+Text.swift
@@ -1,12 +1,17 @@
+//
+//  Robot+Text.swift
+//  DropBear
+//
+//  Created by Ian Keen on 2021-03-10.
+//  Copyright © 2021 Timberlane Labs. All rights reserved.
+//
+
 import XCTest
 
 extension Robot where Self: Actionable {
     public func type(
-        _ element: Element,
-        in hierarchy: [XCUIElement.ElementType] = [.any],
-        text: String,
-        tapFirst: Bool = true,
-        clearFirst: Bool = true,
+        _ element: Element, in hierarchy: [XCUIElement.ElementType] = [.any], text: String,
+        tapFirst: Bool = true, clearFirst: Bool = true,
         file: StaticString = #file, line: UInt = #line
         ) -> Self
     {
@@ -19,8 +24,7 @@ extension Robot where Self: Actionable {
     }
 
     public func clearText(
-        _ element: Element,
-        in hierarchy: [XCUIElement.ElementType] = [.any],
+        _ element: Element, in hierarchy: [XCUIElement.ElementType] = [.any],
         file: StaticString = #file, line: UInt = #line
         ) -> Self
     {
