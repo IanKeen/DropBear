b711d221e29ad287e9cf8dd7eceecb756ffb9ab9
Ian Keen <iankeen82@gmail.com>
2022-08-17 22:12:34 -0700

    Add SwiftUI support to DBGen

--- Sources/DropBearGen/Sources/DropBearGen/Extractor/SyntaxExtractor.swift
+++ Sources/DropBearGen/Sources/DropBearGen/Extractor/SyntaxExtractor.swift
@@ -7,7 +7,7 @@ public final class SyntaxExtractor: Extractor {
     // MARK: - Public Functions
     public func accessibilityIdentifierPairs(for file: Path) throws -> [AccessibilityIdentifierPair] {
         guard file.extension == "swift" else { return [] }
-        
+
         let visitor = Visitor()
         let syntax = try SyntaxParser.parse(file.url)
         visitor.walk(syntax)
@@ -21,15 +21,29 @@ private class Visitor: SyntaxVisitor {
     override func visitPost(_ node: MemberAccessExprSyntax) {
         guard
             node.name.text == "accessibilityIdentifier",
+            let pair = extractUIKit(node) ?? extractSwiftUI(node)
+            else { return }
+
+        pairs.append(pair)
+    }
+
+    private func extractSwiftUI(_ node: MemberAccessExprSyntax) -> AccessibilityIdentifierPair? {
+        guard
+            let tuple = node.parent?._syntaxNode.firstSearchingDown(of: TupleExprElementListSyntax.self),
+            let value = tuple._syntaxNode.firstSearchingDown(of: StringSegmentSyntax.self)?.content.text,
+            let enclosingView = node._syntaxNode.firstSearchingUp(of: StructDeclSyntax.self)
+            else { return nil }
+
+        return .init(parent: enclosingView.identifier.text, identifier: value)
+    }
+    private func extractUIKit(_ node: MemberAccessExprSyntax) -> AccessibilityIdentifierPair? {
+        guard
             let expression = node.parent?.as(ExprListSyntax.self),
             let value = expression._syntaxNode.firstSearchingDown(of: StringSegmentSyntax.self)?.content.text,
             let enclosingClass = node._syntaxNode.firstSearchingUp(of: ClassDeclSyntax.self)
-            else { return }
+            else { return nil }
 
-        pairs.append(.init(
-            parent: enclosingClass.identifier.text,
-            identifier: value
-        ))
+        return .init(parent: enclosingClass.identifier.text, identifier: value)
     }
 }
 
@@ -40,6 +54,18 @@ extension Syntax {
     }
     func firstSearchingDown<T: SyntaxProtocol>(of: T.Type, where predicate: (T) -> Bool = { _ in true }) -> T? {
         if let result = self.as(T.self), predicate(result) { return result }
-        return children.compactMap({ $0.firstSearchingDown(of: T.self, where: predicate) }).first
+
+        var queue: [Syntax] = [self]
+
+        while let item = queue.first {
+            if let result = item.as(T.self), predicate(result) {
+                return result
+            }
+            queue.removeFirst()
+
+            queue.append(contentsOf: item.children.map { $0 })
+        }
+
+        return nil
     }
 }
